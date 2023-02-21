ef5b69e27dfb89f4d38de04ab6596ab1720f1722
AustinC02 <77993889+AustinC02@users.noreply.github.com>
2023-02-21 16:03:38 -0600

    Transfered to using internal PID for rotation

--- arm.py
+++ arm.py
@@ -3,6 +3,9 @@ import wpilib
 import wpimath.controller
 import math
 
+from ctre import FeedbackDevice, ControlMode
+
+
 # vehemently vomiting violent vulgarisms
 # placidly placing purple plaques
 # sadistically spacing sanguine serpents
@@ -10,8 +13,7 @@ import math
 
 
 class ArmedExtension:
-
-    liftMotor: ctre.WPI_TalonFX = None
+    liftMotor: ctre.WPIself.armRFX = None
     extendMotor: ctre.VictorSPX = None
     liftSensors: ctre.TalonFXSensorCollection = None
     startPos = None
@@ -48,61 +50,82 @@ class ArmedExtension:
 
 
 class ArmedRotation:
-
     """
-    TODO Figure out encoder
-     get correct vals
-     calcRotations() function
-     find out how to get length from arm
+    TODO
      implement arm move function
      """
 
     def __init__(self):
-        # variables
-        self.grabbingBay = 0
+        # Variables for use later in code or init; Listed here for easier editing
+        self.BayPos = 0
         self.topPos = 245
         self.midPos = 260
         self.lowPos = 300
-        self.towardBay = -1  # negative or positive one
-        self.towardPlatform = 1  # negative or positive one
-        self.extend = 1
-        self.retract = -1
+        self.kTimeoutMs = 20
+        self.kPIDLoopIdx = 0
+        self.kSlotIdx = 0
+        self.kGains = Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0)
+        self.kSensorPhase = True
+        self.kMotorInvert = False
 
-        self.armR = ctre.WPI_TalonFX(14)  # arm rotation motor number
+        self.armR = ctre.WPIself.armRFX(14)  # arm rotation motor number
+        self.armR.configFactoryDefault()
+        # set stuff
+        self.armR.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, self.kPIDLoopIdx,
+                                               self.kTimeoutMs)
+        # correct the motor encoder just in case
+        self.armR.setSensorPhase(self.kSensorPhase)
+        self.armR.setInverted(self.kMotorInvert)
+        # set peak and nominal outputs 
+        self.armR.configNominalOutputForward(0, self.kTimeoutMs)
+        self.armR.configNominalOutputReverse(0, self.kTimeoutMs)
+        self.armR.configPeakOutputForward(1, self.kTimeoutMs)
+        self.armR.configPeakOutputReverse(-1, self.kTimeoutMs)
+        # allowed closed loop error, it will be neutral within this range
+        self.armR.configAllowableClosedloopError(0, self.kPIDLoopIdx, self.kTimeoutMs)
+        # setting the PID settings
+        self.armR.config_kF(self.kPIDLoopIdx, self.kGains.kF, self.kTimeoutMs)
+        self.armR.config_kP(self.kPIDLoopIdx, self.kGains.kP, self.kTimeoutMs)
+        self.armR.config_kI(self.kPIDLoopIdx, self.kGains.kI, self.kTimeoutMs)
+        self.armR.config_kD(self.kPIDLoopIdx, self.kGains.kD, self.kTimeoutMs)
+        # Grab the 360 degree position of the MagEncoder's absolute position, and initially set the relative sensor
+        # to match
+        absolutePosition = self.armR.getSensorCollection().getPulseWidthPosition()
+
+        # Mask out overflows, keep bottom 12 bits
+        absolutePosition &= 0xFFF
+        if self.kSensorPhase:
+            absolutePosition *= -1
+        if self.kMotorInvert:
+            absolutePosition *= -1
+        # Set the quadrature(relative) sensor to match absolute
+        self.armR.setSelectedSensorPosition(absolutePosition, self.kPIDLoopIdx, self.kTimeoutMs)
+
         self.armRE = wpilib.Encoder(14)
         self.armRE.reset()
 
-    def calcRotations(self, ticks):
-        # do rotation calculation
-        ticksButDegrees = ticks * 4096/360
-        return ticksButDegrees
+        self.lastButton1 = False
 
-    def moveTo(self, pos):
-        RotationInDegrees = self.calcRotations(self.armRE.get())
-        if pos == "grabZone":
-            if RotationInDegrees > self.grabbingBay*2:
-                self.armR.set(self.towardBay)  # For these set() calls, I'm assuming that I'm viewing the robot from
-                # the left (port) side and CCW is negative
-            else:
-                self.armR.set(0)
-        elif pos == "topZone":
-            if RotationInDegrees > self.topPos*2:
-                self.armR.set(self.towardBay)
-            elif RotationInDegrees < self.topPos*2:
-                self.armR.set(self.towardPlatform)
-            else:
-                self.armR.set(0)
-        elif pos == "midZone":
-            if RotationInDegrees > self.midPos*2:
-                self.armR.set(self.towardBay)
-            elif RotationInDegrees < self.midPos*2:
-                self.armR.set(self.towardPlatform)
-            else:
-                self.armR.set(0)
-        elif pos == "lowZone":
-            if RotationInDegrees > self.lowPos*2:
-                self.armR.set(self.towardBay)
-            elif RotationInDegrees < self.lowPos*2:
-                self.armR.set(self.towardPlatform)
-            else:
-                self.armR.set(0)
+    def loop(self, button1, button2, leftYstick):
+
+        if not (self.lastButton1 & button1):
+            # 10 Rotations * 4096 u/rev in either direction
+            targetPositionRotations = leftYstick * 10.0 * 4096
+            self.armR.set(ControlMode.Position, targetPositionRotations)
+            print(targetPositionRotations)
+
+        # When button 2 is held, just straight drive
+        if button2:
+            # Percent Output
+            self.armR.set(ControlMode.PercentOutput, leftYstick)
+        _lastButton1 = button1
+
+
+class Gains:
+    def __init__(self, _kP, _kI, _kD, _kF, _kIzone, _kPeakOutput):
+        self.kP = _kP
+        self.kI = _kI
+        self.kD = _kD
+        self.kF = _kF
+        self.kIzone = _kIzone
+        self.kPeakOutput = _kPeakOutput
