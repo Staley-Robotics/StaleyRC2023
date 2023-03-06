b18810847e69bdd36fdb100d47f23bcdb64712dd
Landon S <112957211+LandonCyber@users.noreply.github.com>
2023-03-06 17:07:53 -0600

    Added code to look at

--- limelightSimple.py
+++ limelightSimple.py
@@ -1,23 +1,17 @@
-from networktables import NetworkTables
+# from networktables import NetworkTables
 
 
 class Limelight:
-
-
-
-    # def __init__(self):
-    #     self.state = NetworkTableInstance.getDefault()
-    #     self.config = self.state.getTable("limelight")
-    #     self.led_state = self.config.getEntry('ledMode')
-    #     self.cam_mode = self.config.getEntry('camMode')
-    #     self.pipeline_index = self.config.getEntry('pipeline')
-    #
-    # def val(self, key):
-    #     return self.config.getEntry(key).getDouble(0.0)
-
+    pass
 
 # table = NetworkTables.getTable("limelight")
 # tx = table.getNumber('tx',None)
 # ty = table.getNumber('ty',None)
 # ta = table.getNumber('ta',None)
 # ts = table.getNumber('ts',None)
+
+# self.state = NetworkTableInstance.getDefault()
+# self.config = self.state.getTable("limelight")
+# self.led_state = self.config.getEntry('ledMode')
+# self.cam_mode = self.config.getEntry('camMode')
+# self.pipeline_index = self.config.getEntry('pipeline')
