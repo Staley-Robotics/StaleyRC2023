dfb43000830699d8d0352f217780d6bd99275486
Landon S <112957211+LandonCyber@users.noreply.github.com>
2023-03-06 17:03:48 -0600

    Deleted most files

---
+++ limelightSimple.py
@@ -0,0 +1,23 @@
+from networktables import NetworkTables
+
+
+class Limelight:
+
+
+
+    # def __init__(self):
+    #     self.state = NetworkTableInstance.getDefault()
+    #     self.config = self.state.getTable("limelight")
+    #     self.led_state = self.config.getEntry('ledMode')
+    #     self.cam_mode = self.config.getEntry('camMode')
+    #     self.pipeline_index = self.config.getEntry('pipeline')
+    #
+    # def val(self, key):
+    #     return self.config.getEntry(key).getDouble(0.0)
+
+
+# table = NetworkTables.getTable("limelight")
+# tx = table.getNumber('tx',None)
+# ty = table.getNumber('ty',None)
+# ta = table.getNumber('ta',None)
+# ts = table.getNumber('ts',None)
