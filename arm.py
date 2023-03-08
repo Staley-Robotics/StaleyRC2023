ee7d616ab2937973a963b7046046bfa8071d1440
AustinC02 <77993889+AustinC02@users.noreply.github.com>
2023-03-07 18:22:57 -0600

    Added a list based position system, also addded incremental adjusting

--- arm.py
+++ arm.py
@@ -26,14 +26,16 @@ class Arm:
     pivot_ratio: int = 200
     degrees_to_ticks = integrated_encoder/360 * pivot_ratio
     pivot_bay: float = 0.0  # needs tuning
-    pivot_low: float = 1024.0  # needs tuning
-    pivot_mid: float = 6228.5  # needs tuning
-    pivot_top: float = 13481.0  # needs tuning
+    pivot_low: float = 10.0 * self.degrees_to_ticks  # needs tuning
+    pivot_mid: float = 45.0 * degrees_to_ticks  # needs tuning
+    pivot_top: float = 95.0 * degrees_to_ticks  # needs tuning
 
     arm_r = WPI_TalonFX(9, "rio")
-    arm_e = WPI_TalonFX(18, "rio")
+    arm_e = WPI_TalonFX(31, "rio")
 
     target_pos: float = 0
+
+    pivot_atm: float = 0
 
     def __init__(self, pipeline: PipelineManager):
         self.pipeline = pipeline
@@ -79,12 +81,28 @@ class Arm:
 
         self.arm_e.selectProfileSlot(0, 0)
 
+        # for list pivot function
+        self.possiblePos = [self.pivot_bay, self.pivot_low, self.pivot_mid, self.pivot_top]
+        self.index = 0
+        self.ad = 0
+        self.curPos = 0
+
     def run_checks(self):
-        self.pivot()
+        self.extend_stickler()
+        self.rotate_stickler()
         # self.extend()
 
-    def stickler(self):
-        self.arm_r.set()
+    def rotate_stickler(self):
+        self.pivot_atm += self.pipeline.pivot_axis() * 0.5 * self.degrees_to_ticks
+        self.pivot_atm -= self.pipeline.pivot_negative_axis() * 0.5 * self.degrees_to_ticks
+        # self.pipeline.pivot_axis() * 90 * self.degrees_to_ticks
+        self.arm_r.set(ControlMode.Position, self.pivot_atm)
+
+    def extend_stickler(self):
+        target = self.pipeline.shaft_axis() * -48 * self.inch_to_ticks
+        if target < -48 * self.inch_to_ticks:
+            target = -48 * self.inch_to_ticks
+        self.arm_e.set(ControlMode.Position, target)
 
     def pivot(self):
         target = 0.0
@@ -101,6 +119,25 @@ class Arm:
         print("Encoder " + str(self.arm_r.getSelectedSensorPosition()))
         print("Target " + str(target))
         print("Screw Up Amount " + str(abs(target - self.arm_r.getSelectedSensorPosition())))
+
+    def listPivot(self):
+        if self.pipeline.point_next:
+            if self.index < self.possiblePos.length:
+                self.index += 1
+                self.ad = 0
+        elif self.pipeline.point_prev:
+            if self.index > 0:
+                self.index -= 1
+                self.ad = 0
+        target = self.possiblePos[self.index]
+        if self.pipeline.point_plus:
+            self.ad += 1 * self.degrees_to_ticks
+        elif self.pipeline.point_minus:
+            self.ad -= 1 * self.degrees_to_ticks
+        target += self.ad
+        self.curPos = possiblePos[self.index]
+        self.arm_r.set(ControlMode.Position, target)
+
 
     def extend(self):
         target = 0.0
