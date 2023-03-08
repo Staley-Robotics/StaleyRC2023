bbce2d9b085ee40c8ec4aac43b2b84f25058bb8a
SHS Robotics <staleyrobotics@gmail.com>
2023-03-08 16:45:35 -0600

    Defense Bot drivetrain with non-working autonomous

--- robot.py
+++ robot.py
@@ -1,79 +1,68 @@
-from typing import List
+import wpilib
+import wpilib.drive
 
-from drivetrain.swerve_drivetrain import *
-from appendage.arm import Arm
-from appendage.claw import Claw
-from autonomous.step import Step
-from drivetrain.chassis import Chassis
-from drivetrain.swerve_raw import Swerve
-from optics.limelight import Limelight
-from tools import PipelineManager, Mode
+from drivetrain import Drivetrain
 
 
-class Robot(TimedRobot):
-    # TODO: Merge and incorporate other classes
-    time: Timer
-    pilot: XboxController
-    other: XboxController
-    pipeline: PipelineManager
-    drivetrain: Chassis
-    limelight: Limelight
-    arm: Arm
-    claw: Claw
-    auto_steps: List[Step]
-    step_index: int
+class Robot(wpilib.TimedRobot):
 
-    def robotInit(self) -> None:
-        self.time = Timer()
-        self.pilot = XboxController(0)
-        self.other = XboxController(1)
-        self.pipeline = PipelineManager(self.pilot, self.other)
-        self.drivetrain = Swerve(self.pipeline)
-        self.limelight = Limelight()
-        self.arm = Arm(self.pipeline)
-        self.claw = Claw(self.pipeline)
-        self.auto_steps = [
-            Step(self.drivetrain.goto, 0.0, 12.0)
-        ]
-        self.step_index = 0
+    time: wpilib.Timer
+    drivetrain: Drivetrain
+    controller1: wpilib.XboxController
+    controller2: wpilib.XboxController
 
-    def robotPeriodic(self) -> None:
-        self.drivetrain.updatePosition()
+    def robotInit(self):
+        self.drivetrain = Drivetrain()
+        self.controller1 = wpilib.XboxController(0)
+        self.controller2 = wpilib.XboxController(1)
+        self.time = wpilib.Timer()
+        # wpilib.CameraServer.launch("limelight.py:launch")
 
-    def teleopInit(self) -> None:
+    def autonomousInit(self):
         self.time.reset()
         self.time.start()
-        self.pipeline.set_mode(Mode.TELEOP)
-        self.claw.compressor.enableDigital()
 
-    def teleopPeriodic(self) -> None:
-        self.drivetrain.drive()
-        self.arm.run_checks()
-        self.claw.run_checks()
-
-    def teleopExit(self) -> None:
+    def autonomousPeriodic(self):
+        pass
+        """
+        if self.time.get() < 1:
+            self.launcher.shooter.set(-0.675)
+            self.launcher.storage.set(1)
+        elif self.time.get() < 2:
+            self.launcher.barrel.set(-1)
+        elif self.time.get() < 3:
+            self.launcher.shooter.set(0)
+            self.launcher.barrel.set(0)
+            self.launcher.storage.set(1)
+            self.launcher.intake.set(1)
+            self.drivetrain.drive(-0.35, 0)
+        elif self.time.get() < 3.5:
+            self.drivetrain.drive(0, 0)
+        elif self.time.get() < 4.5:
+            self.drivetrain.drive(0.35, 0)
+            self.launcher.intake.set(0)
+        elif self.time.get() < 5:
+            self.launcher.shooter.set(-0.675)
+            self.drivetrain.drive(0, 0)
+        elif self.time.get() < 6.5:
+            self.launcher.barrel.set(-1)
+        else:
+            self.launcher.shooter.set(0)
+            self.launcher.barrel.set(0)
+            self.launcher.storage.set(0)
+            """
+    def autonomousExit(self):
         self.time.stop()
 
-    # TODO: Implement Auto
-    def autonomousInit(self) -> None:
-        self.time.reset()
+    def teleopInit(self):
         self.time.start()
-        self.pipeline.set_mode(Mode.AUTO)
 
-    def autonomousPeriodic(self) -> None:
-        if self.auto_steps[self.step_index].callback():
-            self.step_index += 1
+    def teleopPeriodic(self):
+        self.drivetrain.drive(self.controller1.getLeftY(), self.controller1.getLeftX())
 
-    def autonomousExit(self) -> None:
+    def teleopExit(self):
         self.time.stop()
-
-    def testInit(self) -> None:
-        self.pipeline.set_mode(Mode.TELEOP)
-        self.drivetrain = Swerve(self.pipeline)
-
-    def testPeriodic(self) -> None:
-        self.drivetrain.drive()
 
 
 if __name__ == "__main__":
-    run(Robot)
+    wpilib.run(Robot)
